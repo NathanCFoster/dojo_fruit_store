9b75049d0e1227a30749c5347bb8ea0d32da4455
Nathan Foster <nathancfoster.03@gmail.com>
2021-09-03 13:48:15 -0700

    fruits

--- server.py
+++ server.py
@@ -8,7 +8,19 @@ def index():
 @app.route('/checkout', methods=['POST'])         
 def checkout():
     print(request.form)
-    return render_template("checkout.html")
+    charge = int(request.form['strawberry']) + int(request.form['raspberry']) + int(request.form['apple'])
+    print("Charging %s for %d fruits" %(request.form['first_name'], charge))
+    name = request.form['first_name']
+    lastName = request.form['last_name']
+    studentID = request.form['student_id']
+    strawberry = request.form['strawberry']
+    raspberry = request.form['raspberry']
+    apple = request.form['apple']
+    return redirect('/realcheckout/%s/%s/%s/%s/%s/%s' %(name, lastName, studentID, strawberry, raspberry, apple))
+
+@app.route("/realcheckout/<name>/<lastName>/<studentID>/<strawberry>/<raspberry>/<apple>", methods=['Get'])
+def checkedOut(name, lastName, studentID, strawberry, raspberry, apple):
+    return render_template("checkout.html", name=name, lastName=lastName, studentID=studentID, strawberry=strawberry, raspberry=raspberry, apple=apple)
 
 @app.route('/fruits')         
 def fruits():
